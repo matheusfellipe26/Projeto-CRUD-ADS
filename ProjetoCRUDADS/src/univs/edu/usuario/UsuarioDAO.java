0d9317819a89efc767e674ddc653ac2888377af2
Matheus Fellipe <mori.bertoldo@gmail.com>
2019-11-14 21:00:54 -0200

    mori.bertoldo@gmail.com

--- ProjetoCRUDADS/src/univs/edu/usuario/UsuarioDAO.java
+++ ProjetoCRUDADS/src/univs/edu/usuario/UsuarioDAO.java
@@ -2,6 +2,7 @@
 package univs.edu.usuario;
 
 import java.util.List;
+import javax.swing.JOptionPane;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 import org.hibernate.criterion.Restrictions;
@@ -16,8 +17,11 @@ public class UsuarioDAO {
         sessao = HibernateUtil.getSessionFactory().openSession();
         transacao = sessao.beginTransaction();
         if (usuario.getIdUsuario() == 0){
+            sessao.save(usuario);
+            JOptionPane.showMessageDialog(null, "Usuário Cadastrado!");
             }else{
             sessao.update(usuario) ;
+            JOptionPane.showMessageDialog(null, "Usuário Editado!");
         }
         sessao.save(usuario);
         transacao.commit();
